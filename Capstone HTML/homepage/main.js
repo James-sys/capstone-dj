71e798934bf3b8ea5a922db875b6dca90afc01b6
Jordan Hudgens <jordan@devcamp.com>
2018-08-02 10:10:41 -0600

    Nav bar changes

---
+++ Capstone HTML/homepage/main.js
@@ -0,0 +1,9 @@
+console.log("Dark side: If you see this then, you won a cookie!");
+console.log("Light side: Don't belive him he lies, come to the light side");
+console.log("Dark side: We have cookies and cake!");
+console.log(
+  "Light side: We umm have studying the ways to become a jedi for years."
+);
+console.log("Light side: Now that I think about it I like the dark side more");
+console.log("Dark side: Join us");
+console.log("Dark side 2: Join us");
